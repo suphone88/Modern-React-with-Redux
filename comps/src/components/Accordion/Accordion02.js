f4375df53cfd041e026baf9257ad9dbbf77f0985
Dev Su Su <suphonemyint.la@gmail.com>
2023-02-16 23:54:33 +0630

    section12 with dropdown select

---
+++ comps/src/components/Accordion/Accordion02.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function Accordion({ items }) {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+    const handleClick = () => {
+      setExpandedIndex(index);
+    };
+    return (
+      <div key={item.id}>
+        <div onClick={handleClick}>{item.label}</div>
+        {isExpanded && <div> {item.content} </div>}
+      </div>
+    );
+  });
+  return <div> {renderedItems} </div>;
+}
+
+export default Accordion;
