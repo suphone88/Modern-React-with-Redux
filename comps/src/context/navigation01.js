142618acebcf089ce7b86ca5cf5b30d029eb8f18
Dev Su Su <suphonemyint.la@gmail.com>
2023-02-24 23:54:31 +0630

    section17 with custom hooks in depth

---
+++ comps/src/context/navigation01.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { createContext } from "react";
+
+const NavigationContext = createContext();
+
+function NavigationProvider({ children }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener("popstate", handler);
+    return () => {
+      window.removeEventListener("popstate", handler);
+    };
+  }, []);
+
+  const navigate = (to) => {
+    window.history.pushState({}, "", to);
+    setCurrentPath(to);
+  };
+
+  return (
+    <NavigationContext.Provider value={{}}>
+      <div>
+        <button onClick={() => navigate("/accordion")}>
+          {" "}
+          Go to accordion{" "}
+        </button>
+        <button onClick={() => navigate("/dropdown")}> Go to dropdown </button>
+      </div>
+      {currentPath}
+      {children}
+    </NavigationContext.Provider>
+  );
+}
+
+export { NavigationProvider };
+export default NavigationContext;
